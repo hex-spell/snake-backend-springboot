911f44f1fccc7d1882628e8b37f182108dfb5ad0
Modulariz <agustin.favo@gmail.com>
2022-01-14 12:22:51 -0700

    first commit

---
+++ src/main/kotlin/com/modulariz/snakebackendspringboot/SnakeBackendSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.modulariz.snakebackendspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnakeBackendSpringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnakeBackendSpringbootApplication>(*args)
+}
---
+++ src/test/kotlin/com/modulariz/snakebackendspringboot/SnakeBackendSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.modulariz.snakebackendspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnakeBackendSpringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
